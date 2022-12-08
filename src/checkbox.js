7d2dddd6887952a0b654edde65eb48324890858c
Glenda Diaz <glendadiazah@outlook.com>
2022-12-08 14:17:41 -0600

    fix linters

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,34 +1,38 @@
-import {tasks} from './index.js';
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-plusplus */
+/* eslint-disable eqeqeq */
+import { tasks } from './index.js';
 import { reAssingIndex } from './functions.js';
+
 let tasksf = tasks;
 function checkBox(box) {
-    console.log(box);
-    if (box.checked) {
-      let i = 0;
-      while(i!=box.name){
-          i++;
-      }
-      tasks[i].completed = true;
-      tasks[i].chbox = 'checked';
-    } else {
-      let i = 0;
-      while(i!=box.name){
-          i++;
-      }
-      tasks[i].completed = false;
-      tasks[i].chbox = '!checked';
+  if (box.checked) {
+    let i = 0;
+    while (i != box.name) {
+      i++;
     }
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    tasks[i].completed = true;
+    tasks[i].chbox = 'checked';
+  } else {
+    let i = 0;
+    while (i != box.name) {
+      i++;
+    }
+    tasks[i].completed = false;
+    tasks[i].chbox = '!checked';
   }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 const deleteButton = document.querySelector('.clear-btn');
-deleteButton.addEventListener('click',deleteAll);
-function deleteAll(){
-  tasksf=tasks.filter((task) => task.completed !== true);
+
+function deleteAll() {
+  tasksf = tasks.filter((task) => task.completed !== true);
   localStorage.setItem('tasks', JSON.stringify(tasksf));
   reAssingIndex(tasksf);
   localStorage.setItem('tasks', JSON.stringify(tasksf));
   window.location.reload();
 }
+deleteButton.addEventListener('click', deleteAll);
 
-export {checkBox};
+export default checkBox;
